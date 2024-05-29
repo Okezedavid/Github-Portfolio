47f47da399396672b6d50b183390deec82542783
OkezeDavid <davidugochukwu080@gmail.com>
2024-05-29 17:53:38 +0100

    Updated my repodetails page

--- src/Pages/RepoDetails.jsx
+++ src/Pages/RepoDetails.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { FaRegStar, FaRegEye, FaCodeBranch } from 'react-icons/fa';
-import { TbGitFork } from 'react-icons/tb';
-
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { FaRegStar, FaRegEye, FaCodeBranch } from "react-icons/fa";
+import { TbGitFork } from "react-icons/tb";
 
 function MyRepoDetails() {
   const { id } = useParams();
@@ -39,17 +38,38 @@ function MyRepoDetails() {
       <div className="repodetail-card">
         <h2 className="repo-name">{details.name}</h2>
         <div className="repo-mini-details">
-          <p><FaRegStar className="icons" /> Stars: {details.stargazers_count}</p>
-          <p><FaRegEye className="icons" /> Watch: {details.watchers}</p>
-          <p><TbGitFork className="icons" /> Forks: {details.forks}</p>
-          <p><FaCodeBranch className="icons" /> Branches: {branch.length}</p>
+          <p>
+            <FaRegStar className="icons" /> Stars: {details.stargazers_count}
+          </p>
+          <p>
+            <FaRegEye className="icons" /> Watch: {details.watchers}
+          </p>
+          <p>
+            <TbGitFork className="icons" /> Forks: {details.forks}
+          </p>
+          <p>
+            <FaCodeBranch className="icons" /> Branches: {branch.length}
+          </p>
         </div>
-        <p>Main Language: {details.language === null ? 'none' : details.language}</p>
         <p>
-          Live site: {deployment.length === 0 ? 'none' : <a href={`https://Okezedavid.github.io/${details.name}`}>Okezedavid.github.io/{details.name}</a>}
+          Main Language: {details.language === null ? "none" : details.language}
         </p>
         <p>
-          <a href={`https://github.com/${details.full_name}`} target="_blank" rel="noopener noreferrer">
+          Error Page:{" "}
+          {deployment.length === 0 ? (
+            "none"
+          ) : (
+            <a href={`https://Okezedavid.github.io/${details.name}`}>
+              Okezedavid.github.io/{details.name}
+            </a>
+          )}
+        </p>
+        <p>
+          <a
+            href={`https://github.com/${details.full_name}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             View on Github
           </a>
         </p>
